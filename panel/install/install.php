bd5c8611b49ce15ec1abb9878b5a174730a9eba1
YEET200 <67204364+YEET200@users.noreply.github.com>
2022-11-01 15:01:40 -0500

    Update install.php

--- panel/install/install.php
+++ panel/install/install.php
@@ -1,5 +1,27 @@
 <?php
   include('/CONFIG/SMTP.php');
-  include('emailtest.php')
+  include('emailtest.php');
+  require('...\connect.php');
+
+<link rel='stylesheet' href='style.css' />
+
+    $HOST_name = "<input class='input' type='text' name='dbhost' placeholder='Enter Host Name' />";
+    $DB_username = "<input class='input' type='text' name='dbuser' placeholder='Enter DB User Name' />";
+    $DB_password = "<input class='input' type='password' name='dbpass' placeholder='***********' />";
+    $DB_name = "<input class='input' type='text' name='dbname' placeholder='Enter DB Name' />";
+    echo "<div class='box' >
+            <form class='ins' method='post' action='installing.php' >
+                    <p>Enter Host Name:<p>
+                    $HOST_name
+                    <p>Enter DB User Name:<p>
+                    $DB_username
+                    <p>Enter DB PassWord:<p>
+                    $DB_password
+                    <p>Enter DB Name:<p>
+                    $DB_name
+                    <input class='submit' type='submit'name='install' value='NEXT' />
+            </form>
+        </div>";
+?>
     
     
