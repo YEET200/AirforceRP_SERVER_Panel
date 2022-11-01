297fb011a282dd5c5b212243ed45afb87d84a26b
YEET200 <67204364+YEET200@users.noreply.github.com>
2022-11-01 15:13:51 -0500

    Create installed.php

---
+++ panel/install/installed.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST['next'])){
+        /*  enter your
+         sql code for
+         teble creating */
+        echo"<h2 align=center >Finished</h2>";
+    }else{
+        echo"<h2 align=center >Error</h2>";
+    }
+?>
