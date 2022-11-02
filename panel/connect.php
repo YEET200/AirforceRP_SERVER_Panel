91004d613fe232a5588cc01babff4b2ca8a5ccca
YEET200 <67204364+YEET200@users.noreply.github.com>
2022-11-01 20:14:32 -0500

    Update connect.php

--- panel/connect.php
+++ panel/connect.php
@@ -1,6 +1,17 @@
 <?php  // Always Include/Require This File For Connect Database
     require_once "DB_Info.php";  // this is connect.php file
-
+    // PHP MAILER IS NEEDED FOR THIS PART OR YOU CAN TURN OFF EMAIL IN SETTINGS
+    use PHPMailer/PHPMailer/PHPMailer;
+    use PHPMailer/PHPMailer/Exception;
+    require("vendor\autoload.php");
+        $mail->SMTPAuth = true; // There was a syntax error here (SMPTAuth)
+        $mail->SMTPSecure = 'tls';
+        $mail->Host = "smtp.gmail.com";
+        $mail->Mailer = "smtp";
+        $mail->Port = $_POST['SMTP_PORT'];
+        $mail->Username = "YOU@gmail.com";
+        $mail->Password = "YOUR_GMAIL_password";
+       
     @mysqli_connect(
         $HOST_name,
         $DB_username,
@@ -14,4 +25,6 @@
     ) or die(
         "<h2>Database Error, Contact With Admin</h2>"
     );
+
+    
 ?>
